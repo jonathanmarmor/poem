78b48a699f9cd8d47a06dc8b198057755a62167d
andreas jansson <andreas.s.t.jansson@gmail.com>
2014-11-08 22:59:13 -0500

    reverb mp3s; single word played
    Change-Id: I25ec093c82b4b6b42ff9e916e8204f4fc05960ee

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import itertools
 import string
 from collections import defaultdict
 import random
+import redis
 
 from flask import Flask, request, jsonify, render_template
 
@@ -12,58 +13,26 @@ app = Flask(__name__)
 app.secret_key = '\x9d\xc6i:\x04\x1b\x1d\xa7\x8co\xb2\xd4a\x02\xbfM'
 
 
-# DEFAULT_POEM = """Ancient attempts to define poetry such as Aristotle's Poetics focused on the uses of speech in rhetoric drama song and comedy Later attempts concentrated on features such as repetition verse form and rhyme and emphasized the aesthetics which distinguish poetry from more objectively informative prosaic forms of writing"""
-DEFAULT_POEM = 'a as b c cs d ds e f fs g gs'
+app.words = '''
+a photo has no light of its own but it takes light to be seen
+every time anyone takes a photo there is that much less light in circulation
+'''.split()
+
+app.redis_client = redis.Redis()
 
 
-def pairwise(iterable):
-    """Iterate over an iterable two items at a time"""
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return itertools.izip(a, b)
+@app.route('/', defaults={'hash': 'default'})
+@app.route('/<hash>')
+def index(hash):
 
+    word_index = app.redis_client.incr(hash) % len(app.words)
+    previous_word = app.words[(word_index - 1) % len(app.words)]
+    word = app.words[word_index % len(app.words)]
 
-def clean_poem(poem):
-    poem = poem.lower()
-    poem.translate(string.maketrans('', ''), string.punctuation)
-    return poem.split(' ')
-
-
-def make_word_map(poem):
-    word_map = defaultdict(list)
-    poem = clean_poem(poem)
-    for a, b in pairwise(poem):
-        if b not in word_map[a]:
-            word_map[a].append(b)
-    # Make the whole thing circular
-    word_map[poem[-1]].append(poem[0])
-    return poem, word_map
-
-
-app.poem, app.word_map = make_word_map(DEFAULT_POEM)
-
-
-@app.route('/api/word')
-def list_words():
-    return jsonify(words=app.poem)
-
-
-@app.route('/api/word/<previous_word>')
-def get_next(previous_word):
-    next_word = random.choice(app.word_map[previous_word])
-    following_word = random.choice(app.word_map[next_word])
-    return jsonify(
-        previous_word=previous_word,
-        next_word=next_word,
-        following_word=following_word
-    )
-
-
-@app.route('/')
-def index():
     return render_template(
         'index.html',
-        first_word=app.poem[0]
+        previous_word=previous_word,
+        word=word
     )
 
 
